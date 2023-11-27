b210db6cdb5eeb7c965d0d62ac142628cc21b992
jsalva9 <jsalvasoler@hotmail.com>
2023-11-27 21:26:30 +0100

    Add documentation for abstract_module.py

--- src/modules/abstract_module.py
+++ src/modules/abstract_module.py
@@ -5,6 +5,15 @@ from io_manager.io_manager import IO
 
 
 class Module:
+    """
+    Abstract class for modules. All modules should inherit from this class and implement the run method.
+
+    Attributes:
+        _config (Config): the config object.
+        _io (IO): the IO object.
+        _records (list): the records.
+        _time_limit (int): the time limit.
+    """
     def __init__(self, config: Config, io: IO):
         self._config = config
         self._io = io
